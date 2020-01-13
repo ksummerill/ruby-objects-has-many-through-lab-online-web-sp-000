90c559c49c3a7d21bf7046353d4169e1668dc17c
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 16:00:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,5 +13,16 @@ class Patient
     @@all
   end
 
+  # takes in an argument of a doctor and a date and creates a new Appointment.
+  # The Appointment should know that it belongs to the patient.
+  def new_appointment(doctor, date)
+
+  end
+
+  # iterates through the Appointments array and
+  # returns Appointments that belong to the patient.
+  def appointments
+
+  end
 
 end
