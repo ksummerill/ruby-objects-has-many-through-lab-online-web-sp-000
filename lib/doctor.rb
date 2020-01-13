a7fb8a5c343efd07abb8248c6a63bdb34cbe0494
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 15:58:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,4 +13,21 @@ class Doctor
     @@all
   end
 
+  # takes in a an instance of the Patient class and a date, and creates a new Appointment.
+  # That Appointment should know that it belongs to the doctor
+  def new_appointment(Patient, date)
+
+  end
+
+  # iterates through all Appointments and finds those belonging to this doctor.
+  def appointments
+
+  end
+
+  # iterates over that doctor's Appointments and
+  # collects the patient that belongs to each Appointments.
+  def patients
+
+  end
+
 end
