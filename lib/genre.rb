c358e45e9cc31cd03a86bdd078d67ea89a0f487d
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 15:18:35 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,7 +15,7 @@ class Genre
 
   # iterates through all songs and finds the songs that belong to that genre.
   def songs
-    Song.all.find {|songs| song.genre == self }
+    Song.all.find {|songs| songs.genre == self }
   end
 
   # iterates over the genre's collection of songs and collects the artist that owns each song.
