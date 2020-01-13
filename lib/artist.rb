7efa5051dce67a48bc97c65bda5eb5bffbe25a6e
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 15:15:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
   # given a name and genre, creates a new song associated with that artist
   def new_song(name, genre)
     binding.pry
-    self.name = Song.new(name, self, genre)
+    self = Song.new(name, self, genre)
   end
 
   def songs
