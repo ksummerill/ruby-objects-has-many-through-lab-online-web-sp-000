054cd56a4704d84ff663f7f51a3bc7ac11a43f5f
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 16:36:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -30,7 +30,7 @@ class Patient
   # collects the doctor that belongs to each Appointment.
   def doctors
     binding.pry
-    appointments.collect {|appointment| appointment.doctor}
+    appointments.doctor.collect {|appointment| appointment.doctor}
   end
 
 end
