0e3c009c3670eda2fd5027436d3ca04af3b51b63
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 15:38:46 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -23,7 +23,7 @@ class Genre
   def artists
     binding.pry
     # Song.all.select {|artist| artist.genre == self}
-    Song.all.select {|artist| Song.name.genre == self}
+    Song.all.select {|artist| Song.name.artist == self}
   end
 
 
