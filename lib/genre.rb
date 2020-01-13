c84ecd2cc95a9303c96fd19e41e7ead7ac674840
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-13 15:30:59 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -22,7 +22,7 @@ class Genre
   # iterates over the genre's collection of songs and collects the artist that owns each song.
   def artists
     binding.pry
-    Song.all.collect {|artist| artist.artist == self}
+    Song.all.select {|artist| artist.artist == self}
   end
 
 
